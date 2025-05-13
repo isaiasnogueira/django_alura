0d037fd7ade0e2d9f337f10b8df688b9ea265d7e
isaiasnogueira <insantosdev@gmail.com>
2025-05-13 07:50:45 -0300

    atualizando app setup

--- setup/settings.py
+++ setup/settings.py
@@ -41,7 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'galeria',
+    
+]   
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
